15b51827e740d811a3f2eca7f388c45fe3f5dcd9
Smoodust <42073101+Smoodust@users.noreply.github.com>
2024-07-29 17:23:34 +0400

    Changed to null-friendly

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def get_user_by_username(username: str):
     with Session(engine) as session:
         statement = select(User).where(User.username == username)
         results = session.exec(statement)
-        result = results.one()
+        result = results.first()
         return result
 
 
@@ -104,7 +104,7 @@ def get_user_by_email(email: str):
     with Session(engine) as session:
         statement = select(User).where(User.email == email)
         results = session.exec(statement)
-        result = results.one()
+        result = results.first()
         return result
 
 
