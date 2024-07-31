2de001d86e4c08844c12d244b8a69fc1db7ef51a
Smoodust <42073101+Smoodust@users.noreply.github.com>
2024-07-31 19:58:41 +0400

    Made buy form button work

--- js/account_navbar.js
+++ js/account_navbar.js
@@ -3,18 +3,18 @@ var is_shown_navbar_menu = false;
 
 function show_navbar_menu() {
     account_navbar_menu.classList.remove('no-seen');
+    is_shown_navbar_menu = true;
 }
 
 function hide_navbar_menu() {
     account_navbar_menu.classList.add('no-seen');
+    is_shown_navbar_menu = false;
 }
 
 function toggle_navbar_menu() {
     if (is_shown_navbar_menu) {
         hide_navbar_menu();
-        is_shown_navbar_menu = false;
     } else {
         show_navbar_menu();
-        is_shown_navbar_menu = true;
     }
 }
