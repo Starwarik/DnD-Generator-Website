f68211b6143a50829d8ce351ff63f865cc9dfa47
Smoodust <42073101+Smoodust@users.noreply.github.com>
2024-07-31 17:51:53 +0400

    Added confirmation of senting email

--- js/dialogue_managment.js
+++ js/dialogue_managment.js
@@ -2,11 +2,14 @@ const dialog_register = document.getElementById('dialog-register');
 const dialog_login = document.getElementById('dialog-login');
 const dialog_reset_password = document.getElementById('dialog-reset-password');
 const dialog_reset_password_second = document.getElementById('dialog-reset-password-second');
+const dialog_reset_password_letter = document.getElementById('dialog-reset-password-letter');
+
 
 dialog_register.addEventListener('mousedown', () => dialog_register.close());
 dialog_login.addEventListener('mousedown', () => dialog_login.close());
 dialog_reset_password.addEventListener('mousedown', () => dialog_reset_password.close());
 dialog_reset_password_second.addEventListener('mousedown', () => dialog_reset_password_second.close());
+dialog_reset_password_letter.addEventListener('mousedown', () => dialog_reset_password_letter.close());
 
 
 let dialog_viewports = document.getElementsByClassName("dialog-viewport");
@@ -20,6 +23,7 @@ function close_all_dialogues() {
     dialog_login.close();
     dialog_reset_password.close();
     dialog_reset_password_second.close();
+    dialog_reset_password_letter.close();
 }
 
 function show_login() {
@@ -35,6 +39,11 @@ function show_register() {
 function show_reset_password() {
     close_all_dialogues();
     dialog_reset_password.showModal();
+}
+
+function show_reset_password_letter() {
+    close_all_dialogues();
+    dialog_reset_password_letter.showModal();
 }
 
 function show_reset_password_second() {
--- js/forms.js
+++ js/forms.js
@@ -39,7 +39,7 @@ dialog_form_reset_password.addEventListener('submit', async (e) => {
     const email = formData.get('email');
 
     sendResetLetter(email);
-    location.reload();
+    show_reset_password_letter();
 });
 
 dialog_form_reset_password_second.addEventListener('submit', async (e) => {
