318c39a760b5b81c67a5e0f7d4c7ff685f5b5d09
Smoodust <42073101+Smoodust@users.noreply.github.com>
2024-07-31 19:05:08 +0400

    Added updating user info. Changed text when loading

--- js/account.js
+++ js/account.js
@@ -2,9 +2,12 @@ const account_navbar_noname = document.getElementById('account-navbar-noname');
 const account_navbar_auth = document.getElementById('account-navbar-auth');
 
 async function setUserInfo(token) {
+    account_navbar_auth.classList.remove('no-seen');
     const user_info = await getUserInfo(token);
     document.getElementById('account-navbar-auth__login').textContent = user_info.username;
-    account_navbar_auth.classList.remove('no-seen');
+    document.getElementById('account-navbar-menu__login').textContent = user_info.username;
+    document.getElementById('account-navbar-menu__email').textContent = user_info.email;
+    document.getElementById('account-navbar-menu__balance').textContent = user_info.balance;
 }
 
 const token = getCookie('access_token');
