ed671e64845446c8b889bf05c970bd6cadd8b3e6
Smoodust <42073101+Smoodust@users.noreply.github.com>
2024-08-01 21:12:24 +0400

    Changed to accept body

--- main.py
+++ main.py
@@ -37,6 +37,10 @@ class UserRegisterForm(BaseModel):
     password: str  
     email: str
 
+class RestPasswordForm(BaseModel):  
+    new_password: str  
+    token_reset: str  
+
 
 class Token(BaseModel):
     access_token: str
@@ -208,14 +212,14 @@ def get_user_info(
     }
 
 @app.post("/api/reset_password")
-def reset_password(new_password: str, token_reset: str):
+def reset_password(resest_form: RestPasswordForm):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token_reset, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(resest_form.token_reset, SECRET_KEY, algorithms=[ALGORITHM])
         username: str | None = payload.get("sub")
         if username is None or payload.get("type") != "reset":
             raise credentials_exception
@@ -226,7 +230,7 @@ def reset_password(new_password: str, token_reset: str):
     if user is None:
         raise credentials_exception
     with Session(engine) as session:
-        user.password = get_password_hash(new_password)
+        user.password = get_password_hash(resest_form.new_password)
         session.add(user)
         session.commit()  
         session.refresh(user)
