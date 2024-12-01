651799d6bc18fbfc9c8017390e19e6e58a49c65d
Smoodust <42073101+Smoodust@users.noreply.github.com>
2024-12-01 20:24:27 +0400

    Deleted moved functions

--- backend/app/user/router.py
+++ backend/app/user/router.py
@@ -17,27 +17,3 @@ async def get_user_info(current_user: Annotated[User, Depends(get_current_user)]
         "email": current_user.email,
         "balance": current_user.balance,
     }
-
-
-if app_setting.is_test:
-
-    @user_router.get("/api/get_users")
-    async def get_all_users(session: AsyncSession = Depends(get_session)):
-        statement = select(User)
-        results = await session.execute(statement)
-        result = results.scalars().all()
-        return result
-
-    @user_router.post("/api/infinite_money/{user_id}")
-    async def get_infinite_money(
-        user_id: int, session: AsyncSession = Depends(get_session)
-    ):
-        statement = (
-            update(User)
-            .where(User.id == user_id)
-            .values(balance=99999999999999999999999999)
-        )
-        await session.execute(statement)
-        await session.commit()
-=======
->>>>>>> main
